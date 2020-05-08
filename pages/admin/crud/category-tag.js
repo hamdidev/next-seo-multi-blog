7296a49842a6753f53d25744ecbd2a53b9704101
hamdidev <itechtranss@gmail.com>
2020-05-08 23:50:35 +0300

    Tags created

--- pages/admin/crud/category-tag.js
+++ pages/admin/crud/category-tag.js
@@ -2,6 +2,7 @@ import Layout from "../../../components/Layouts";
 
 import Admin from "../../../components/auth/Admin";
 import Category from "../../../components/crud/Category";
+import Tag from "../../../components/crud/Tag";
 
 const CategoryTag = () => {
   return (
@@ -16,7 +17,7 @@ const CategoryTag = () => {
               <Category />
             </div>
             <div className='col-md-6'>
-              <p>Tags</p>
+              <Tag />
             </div>
           </div>
         </div>
